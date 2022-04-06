c0ced9477780aea76bd5241e25c4f131e95b21e8
Mehmet Batuhan Duman <58864224+bthndmn12@users.noreply.github.com>
2022-04-06 14:16:37 +0300

    isDivisibleProblemSolution

---
+++ src/divisible/Divisible.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package divisible;
+
+/**
+ *
+ * @author admin
+ */
+public class Divisible {
+ public static void main(String[] args) {
+        System.out.println(is_divisible(0));
+       
+    }
+    
+    public static String is_divisible(int a){
+        
+        int sumOfDigits = 0;
+        char n;
+        if (a != 0) {
+            
+            for (int i = 0; i < String.valueOf(a).length() ; i++) {
+             n = String.valueOf(a).charAt(i);
+             sumOfDigits += Character.getNumericValue(n);
+        }
+            if (a % sumOfDigits == 0) {
+            return "True";
+        }
+        }
+        
+        
+        return "False";
+    }
+}
